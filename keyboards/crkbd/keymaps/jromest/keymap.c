df09d3a7342c30195113afdcc4487f462669b00b
jromest <jromestiller@gmail.com>
2024-11-05 22:58:29 +0800

    feat(corne): ✨ move backspace to left thumb key

--- keyboards/crkbd/keymaps/jromest/keymap.c
+++ keyboards/crkbd/keymaps/jromest/keymap.c
@@ -40,49 +40,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
   [_QWERTY] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_RCTL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            LT(_LOWER, KC_BTN1),  KC_LCTL,  KC_BTN1,    KC_SPC,  KC_LGUI, LT(_RAISE, KC_LALT)
+                            LT(_LOWER, KC_BTN1),  KC_LCTL,  KC_BSPC,    KC_SPC,  KC_LGUI, LT(_RAISE, KC_LALT)
                         //`----------------------------------------'  `--------------------------------------'
   ),
 
   [_COLEMAK_DH] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
+       KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                         KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_CAPS,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                         KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                         KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            LT(_LOWER, KC_BTN1),  KC_LCTL,  KC_BTN1,    KC_SPC,  KC_LGUI, LT(_RAISE, KC_LALT)
+                            LT(_LOWER, KC_BTN1),  KC_LCTL,  KC_BSPC,    KC_SPC,  KC_LGUI, LT(_RAISE, KC_LALT)
                         //`----------------------------------------'  `--------------------------------------'
   ),
 
 [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, XXXXXXX, XXXXXXX,   KC_UP,  XXXXXXX, KC_PGUP,                     KC_HOME,  KC_7,   KC_8,    KC_9,  XXXXXXX, KC_BSPC,
+       KC_TAB, KC_ESC, XXXXXXX,   KC_UP,  XXXXXXX, KC_PGUP,                     KC_HOME,   KC_7,   KC_8,    KC_9,  XXXXXXX,  KC_RCTL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_CAPS, CW_TOGG, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN,                     KC_END,   KC_4,   KC_5,    KC_6,  XXXXXXX,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_ESC,                       KC_0,   KC_1,   KC_2,    KC_3,  XXXXXXX, RSFT_T(KC_ENT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_TRNS,  KC_LCTL,  KC_BTN1,    KC_SPC,  KC_LGUI, LT(_RAISE, KC_LALT)
+                                        KC_TRNS,  KC_LCTL,  KC_BSPC,    KC_SPC,  KC_LGUI, LT(_RAISE, KC_LALT)
                                     //`----------------------------'  `--------------------------------------'
   ),
 
   [_RAISE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+       KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_RCTL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                            LT(_LOWER, KC_BTN1),  KC_LCTL,  KC_BTN1,    KC_SPC,  KC_LGUI, KC_TRNS
+                            LT(_LOWER, KC_BTN1),  KC_LCTL,  KC_BSPC,    KC_SPC,  KC_LGUI, KC_TRNS
                         //`----------------------------------------'  `--------------------------'
   ),
 
