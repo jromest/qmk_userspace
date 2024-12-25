625461b8135ea64a4b10f4cecc139a2f75c54683
jromest <jromestiller@gmail.com>
2024-12-25 21:19:14 +0800

    feat(corne): 🔥 remove oled keylogger

--- keyboards/crkbd/keymaps/jromest/keymap.c
+++ keyboards/crkbd/keymaps/jromest/keymap.c
@@ -98,7 +98,7 @@ void oled_render_layer_state(void) {
 
     switch (get_highest_layer(layer_state)) {
         case _COLEMAK_DH:
-            oled_write_ln_P(PSTR("COLEMAK-DH"), false);
+            oled_write_ln_P(PSTR("Base"), false);
             break;
         case _LOWER:
             oled_write_ln_P(PSTR("Lower"), false);
@@ -111,50 +111,6 @@ void oled_render_layer_state(void) {
             break;
         default:
             oled_write_ln_P(PSTR("Undef"), false);
-    }
-}
-
-
-char keylog_str[24] = {};
-
-const char code_to_name[60] = {
-    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
-
-void set_keylog(uint16_t keycode, keyrecord_t *record) {
-  char name = ' ';
-    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
-  if (keycode < 60) {
-    name = code_to_name[keycode];
-  }
-
-  // update keylog
-  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-           record->event.key.row, record->event.key.col,
-           keycode, name);
-}
-
-void oled_render_keylog(void) {
-    oled_write(keylog_str, false);
-}
-
-void render_bootmagic_status(bool status) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    if (status) {
-        oled_write_ln_P(logo[0][0], false);
-        oled_write_ln_P(logo[0][1], false);
-    } else {
-        oled_write_ln_P(logo[1][0], false);
-        oled_write_ln_P(logo[1][1], false);
     }
 }
 
@@ -180,19 +136,9 @@ bool oled_task_user(void) {
         } else {
             oled_write_ln_P(PSTR("Caps Word: OFF"), false);
         }
-
-        oled_render_keylog();
     } else {
         oled_render_logo();
     }
     return false;
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        set_keylog(keycode, record);
-    }
-
-    return true;
 }
 #endif // OLED_ENABLE
